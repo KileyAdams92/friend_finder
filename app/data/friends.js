16052a603fae2e955f8738e43f915b76821057d1
Kiley Adams <Kileyadams92@gmail.com>
2018-04-19 18:10:39 -0500

    modifed API route files

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,31 +15,3 @@ var people = [
     scores: ["5", "4", "4", "3", "2", "5", "3", "3", "1", "5"]
   }
 ];
-
-$("#add-btn").on("click", function(event) {
-  event.preventDefault();
-  var newFriend = {
-    name: $("#newFriendName")
-      .val()
-      .trim(),
-    photo: $("#newPhotoLink")
-      .val()
-      .trim(),
-    scores: [
-      $("#q1").val(),
-      $("#q2").val(),
-      $("#q3").val(),
-      $("#q4").val(),
-      $("#q5").val(),
-      $("#q6").val(),
-      $("#q7").val(),
-      $("#q8").val(),
-      $("#q9").val(),
-      $("#q10").val()
-    ]
-  };
-
-  $.post("/api/new", newFriend).then(function(data) {
-    console.log(data);
-  });
-});
